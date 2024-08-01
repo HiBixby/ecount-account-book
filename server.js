dc733a6752d623fca2c4ee32ca216201eabb97f7
HiBixby <fpssj@naver.com>
2024-08-01 18:06:18 +0900

    feat: cors 관련 문제 해결

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/view', (req, res) => {
+    const data = [
+        {
+            id: 1,
+            time: "날짜1",
+            asset: "카카오뱅크",
+            type: "식비",
+            content: "외식",
+            price: "50000"
+        },
+        {
+            id: 2,
+            time: "날짜2",
+            asset: "카카오뱅크",
+            type: "식비",
+            content: "외식",
+            price: "-50000"
+        },
+
+    ]
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json(data);
 })
 
 app.listen(5000)
