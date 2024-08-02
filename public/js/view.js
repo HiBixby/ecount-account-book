7f0fe9fe1a1608d89e9a2554deefcae47249ba12
HiBixby <fpssj@naver.com>
2024-08-02 16:08:20 +0900

    feat: 월 변경 컨트롤러 기능 추가

--- public/js/view.js
+++ public/js/view.js
@@ -36,11 +36,11 @@ async function renderHistory() {
                 <td class="${data.price > 0 ? `plus` : `minus`}">${commaizeNumber(Math.abs(data.price)) + "원"}</td>`
         table.insertBefore(tr, null);
     }
-    //전체 카운트, 전체 자산 설정
+    //전체 건수, 전체 금액 설정
     const totalCntElement = document.querySelector(".view .total-cnt");
     totalCntElement.innerHTML = response.length;
     const totalAmountElement = document.querySelector(".view .total-amount");
-    totalAmountElement.innerHTML = commaizeNumber(totalAmount);
+    totalAmountElement.innerHTML = commaizeNumber(Math.abs(totalAmount));
     if (totalAmount > 0) totalAmountElement.parentElement.className = "plus"
     else totalAmountElement.parentElement.className = "minus"
     // 수입 건수, 수입 금액 설정
@@ -52,6 +52,36 @@ async function renderHistory() {
     const outcomeCntElement = document.querySelector(".view .outcome-cnt");
     outcomeCntElement.innerHTML = outcomeCnt;
     const outcomeAmountElement = document.querySelector(".view .outcome-amount");
-    outcomeAmountElement.innerHTML = commaizeNumber(outcomeAmount);
+    outcomeAmountElement.innerHTML = commaizeNumber(Math.abs(outcomeAmount));
+}
+
+let current = new Date();
+
+function formatDate() {
+    let year = current.getFullYear();
+    let month = `0${current.getMonth() + 1}`.slice(-2);
+    let yyyymm = year + "-" + month;
+    return yyyymm;
+}
+function renderMonth() {
+    document.querySelector(".view .month").innerHTML = formatDate();
+
+}
+function showPrevMonth() {
+    current.setMonth(current.getMonth() - 1);
+    renderMonth();
+}
+function showNextMonth() {
+    current.setMonth(current.getMonth() + 1);
+    renderMonth();
+}
+function showThisMonth() {
+    current = new Date();
+    renderMonth();
 }
 renderHistory();
+renderMonth();
+
+document.querySelector(".view .button-left").addEventListener("click", showPrevMonth);
+document.querySelector(".view .button-right").addEventListener("click", showNextMonth);
+document.querySelector(".view .reset-month").addEventListener("click", showThisMonth);
