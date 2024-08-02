9a86d8af85011bb782a0dcc156a3d04cd830e4c7
devedu8 <devedu@naver.com>
2024-08-02 17:05:44 +0900

    feat: 내역 테이블 db 연결

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'household_db'
+    database: 'household_db',
+    "timezone": "Asia/Seoul",
+    "dateStrings": true
 });
 
 connection.connect();
@@ -34,14 +36,48 @@ app.get('/account', (req, res) => {
         },
 
     ]
-    connection.query('select * from household_db.category', (error, rows, fields) => {
+    const query =
+        `
+    SELECT 
+        h.id AS id,
+        h.transaction_date,
+        h.asset,
+        h.description as content,
+        CASE 
+            WHEN h.type = 'income' THEN h.amount
+            WHEN h.type = 'expense' THEN -h.amount
+            ELSE h.amount  -- 예외 처리: 타입이 income 또는 expense가 아닐 경우
+        END AS price,
+        c.name AS type
+        FROM 
+            household_account AS h
+        JOIN 
+            category AS c ON h.category_id = c.id;
+    `;
+
+    connection.query(query, (error, results, fields) => {
         if (error) throw error;
-        console.log('User info is: ', rows);
+        const formattedResults = results.map(row => {
+            const date = new Date(row.transaction_date);
+            console.log(date);
+            return {
+                id: row.id,
+                transaction_date: date,
+                asset: row.asset,
+                content: row.content,
+                price: row.price,
+                type: row.type
+            }
+        });
+
+        console.log(formattedResults);
+
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.json(formattedResults);
+
     });
 
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json(data);
 })
 
 app.listen(5000)
