2c039fbe8ae5a2c58413ec5062cb2555e5df4d33
sohyun215 <kshyun0215@gmail.com>
2024-08-02 18:44:22 +0900

    feat: 하위 카테고리 조회 api 작성

---
+++ backend/route/category.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const db = require('../lib/db');
+
+const router = express.Router();
+
+/** 하위 카테고리 가져오기 */
+router.get('/sub', (req, res) => {
+  const { type } = req.query;
+
+  // type이 수입(income) or 지출(expense)이 아닐 경우 error
+  if (!type || !['income', 'expense'].includes(type)) {
+    return res.status(400).send('타입을 확인해주세요');
+  }
+
+  const query =
+    'SELECT * FROM category WHERE parent_id IS NOT NULL AND type = ?';
+
+  db.query(query, type, (err, results) => {
+    if (err) {
+      return res.status(500).send('Database error');
+    }
+    res.send(results);
+  });
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,15 @@
 const express = require('express');
 
 const transactions = require('./route/transactions');
+const category = require('./route/category');
 
 const app = express();
 
 app.use(express.json());
 app.use('/account', transactions);
+app.use('/category', category);
 
 const PORT = 5000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
