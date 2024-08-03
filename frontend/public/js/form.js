6efc6c125600153ffd0005be46ea437c1def7a85
sohyun <kshyun0215@gmail.com>
2024-08-03 10:59:35 +0900

    feat: 상세 내역 가져와서 폼 초기화, 내역 저장 후 테이블 업데이트

--- frontend/public/js/form.js
+++ frontend/public/js/form.js
@@ -2,13 +2,14 @@ import {
   addTransaction,
   deleteTransaction,
   getCategories,
+  getTransactionDetail,
   updateTransaction,
 } from '../api.js';
+import { renderHistory } from './view.js';
 
 const form = document.forms.transaction;
 
-const params = new URLSearchParams(window.location.search);
-const transactionId = params.get('transaction_id');
+let transactionId = null;
 
 const deleteButton = document.getElementById('delete-btn');
 const addButton = document.querySelector('.btn-add-transaction');
@@ -19,32 +20,63 @@ const floatingButtonContainer = document.querySelector(
   '.floating-btn-container',
 );
 
-function initForm() {
-  // TODO: 목록에서 내역 클릭 시 해당 내역 상세 불러오기
-
-  renderCategory();
-
+async function initForm() {
   if (!transactionId) {
     deleteButton.disabled = true;
+    renderCategory();
+  } else {
+    try {
+      const {
+        type,
+        transaction_date,
+        asset,
+        category_id,
+        amount,
+        description,
+        memo,
+      } = await getTransactionDetail(transactionId);
+
+      document.querySelector(
+        `input[name="transaction_type"][value="${type}"]`,
+      ).checked = true;
+      // 수입 / 지출 타입 확인 후 분류 다시 가져와서 렌더링
+      await renderCategory();
+
+      document.getElementById('date').value = transaction_date.slice(0, 10);
+      document.getElementById('time').value = transaction_date.slice(11, 16);
+      document.getElementById('asset').value = asset;
+      document.getElementById('category').value = category_id;
+      document.getElementById('amount').value = amount;
+      document.getElementById('description').value = description;
+      document.getElementById('memo').value = memo;
+    } catch (error) {
+      alert('상세 내역을 불러오는데 실패했습니다.');
+    }
   }
 }
-
-initForm();
 
 async function renderCategory() {
   const type = document.querySelector(
     'input[name="transaction_type"]:checked',
   ).value;
+
   const categorySelect = document.getElementById('category');
+  try {
+    const categories = await getCategories(type);
 
-  const categories = await getCategories(type);
+    // 기존 존재하는 분류 옵션 제거, 기본 옵션 추가
+    categorySelect.innerHTML =
+      '<option value="none" selected disabled hidden>분류 선택</option>';
 
-  categories.forEach(category => {
-    const option = document.createElement('option');
-    option.value = category.id;
-    option.textContent = category.name;
-    categorySelect.appendChild(option);
-  });
+    categories.forEach(category => {
+      const option = document.createElement('option');
+      option.value = category.id;
+      option.textContent = category.name;
+      categorySelect.appendChild(option);
+    });
+  } catch (error) {
+    alert('카테고리 목록을 불러오는데 실패했습니다.');
+  }
 }
 
 async function submitHandler(event) {
@@ -61,6 +93,11 @@ async function submitHandler(event) {
     'input[name="transaction_type"]:checked',
   ).value;
 
+  if (categoryId === 'none') {
+    alert('분류를 선택해주세요.');
+    return;
+  }
+
   const transactionData = {
     transaction_date: `${date} ${time}`,
     category_id: categoryId,
@@ -72,30 +109,65 @@ async function submitHandler(event) {
   };
 
   // 수입 지출 새로 추가
-  if (!transactionId) {
-    addTransaction(transactionData);
-  } else {
-    updateTransaction(transactionId, transactionData);
+  try {
+    if (!transactionId) {
+      await addTransaction(transactionData);
+    } else {
+      await updateTransaction(transactionId, transactionData);
+    }
+
+    renderHistory();
+    closeInputForm();
+  } catch (error) {
+    alert('내역 저장에 실패했습니다. 다시 시도해주세요.');
   }
 }
 
 async function deleteHandler() {
-  await deleteTransaction(transactionId);
-  alert('삭제 완료');
+  try {
+    await deleteTransaction(transactionId);
+
+    alert('삭제가 완료되었습니다.');
+    renderHistory();
+    closeInputForm();
+  } catch (error) {
+    alert('삭제에 실패했습니다. 다시 시도해주세요.');
+  }
 }
+
+function openInputForm() {
+  aside.classList.add('open');
+  view.style.marginRight = '440px';
+  floatingButtonContainer.style.right = '460px';
+  initForm();
+}
+
+function closeInputForm() {
+  aside.classList.remove('open');
+  view.style.marginRight = '0';
+  floatingButtonContainer.style.right = '20px';
+  transactionId = null; // 닫을 때 id 초기화
+  form.reset();
+}
+
+const radioButtons = document.querySelectorAll(
+  'input[name="transaction_type"]',
+);
+radioButtons.forEach(radio => {
+  radio.addEventListener('change', renderCategory); // 수입, 지출 선택 바꿀 때마다 카테고리 목록 가져오기
+});
 
 form.addEventListener('submit', submitHandler);
 deleteButton.addEventListener('click', deleteHandler);
 
 // 폼 열고 닫는 이벤트
-addButton.addEventListener('click', () => {
-  aside.classList.add('open');
-  view.style.marginRight = '440px';
-  floatingButtonContainer.style.right = '460px';
-});
+addButton.addEventListener('click', openInputForm);
+formCloseButton.addEventListener('click', closeInputForm);
 
-formCloseButton.addEventListener('click', () => {
-  aside.classList.remove('open');
-  view.style.marginRight = '0';
-  floatingButtonContainer.style.right = '20px';
+document.querySelector('.view table').addEventListener('click', event => {
+  const tr = event.target.closest('tr');
+  if (tr && tr.dataset.id) {
+    transactionId = tr.dataset.id;
+    openInputForm();
+  }
 });
